901101d4625b75d4d74284ce22db05ff5f12b5b4
Poovarasan <mobtech.github@gmail.com>
2022-08-20 07:56:57 +0530

    Fixed policy url intent crash issue

--- app/src/main/java/com/mobtech/xo_offline_game/Utils/Utils.kt
+++ app/src/main/java/com/mobtech/xo_offline_game/Utils/Utils.kt
@@ -1,9 +1,10 @@
+@file:Suppress("DEPRECATION")
+
 package com.mobtech.xo_offline_game.Utils
 
 import android.content.Context
 import android.preference.PreferenceManager
 
-@Suppress("DEPRECATION")
 class Utils {
     fun setPref(c: Context, pref: String, value: Boolean) {
         val e = PreferenceManager.getDefaultSharedPreferences(c).edit()
